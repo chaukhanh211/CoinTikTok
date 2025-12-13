af08fa0fc2a357a0dfd3e7713e2abc93a22443b2
chaukhanh211 <chaukhanh211@gmail.com>
2025-12-13 22:10:55 +0700

    f

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
+const CACHE_NAME = 'cointiktok-v1';
+
 self.addEventListener('install', e => {
   self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
